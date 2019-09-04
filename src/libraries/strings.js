d1e52baff4336d57950ba9b40f212447d5ca5f58
Eugene Teplitsky <eugene@wishknish.com>
2019-09-04 18:26:29 -0400

    Adding missing copyright header

--- src/libraries/strings.js
+++ src/libraries/strings.js
@@ -1,3 +1,8 @@
+// Copyright 2019 WishKnish Corp. All rights reserved.
+// You may use, distribute, and modify this code under the GPLV3 license, which is provided at:
+// https://github.com/WishKnish/KnishIO-Client-JS/blob/master/LICENSE
+// This experimental code is part of the Knish.IO API Client and is provided AS IS with no warranty whatsoever.
+
 import bigInt from 'big-integer';
 import LZString from 'lz-string';
 
