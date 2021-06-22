afbdcd3d77b28b80dfcc64d15aae81defaf492c1
Zaur <len1in@yandex.ru>
2021-06-22 20:41:10 +0300

    Completion of the Active Browsing Sessions task according to the discussions during calls

---
+++ src/query/QueryUserActivity.js
@@ -0,0 +1,48 @@
+import Query from './Query';
+import ResponseQueryUserActivity from '../response/ResponseQueryUserActivity';
+import gql from 'graphql-tag';
+
+
+export default class QueryUserActivity extends Query {
+  /**
+   * Class constructor
+   *
+   * @param httpClient
+   */
+  constructor ( httpClient ) {
+    super( httpClient );
+
+    this.$__query = gql`query UserActivity ($bundleHash:String, $metaType: String, $metaId: String, $countBy: CountByUserActivity, $interval: span) {
+        UserActivity (bundleHash: $bundleHash, metaType: $metaType, metaId: $metaId, countBy: $countBy, interval: $interval ) {
+            createdAt,
+            bundleHash,
+            metaType,
+            metaId,
+            instances {
+                bundleHash,
+                metaType,
+                metaId,
+                jsonData,
+                createdAt,
+                updatedAt
+            },
+            instanceCount {
+                key,
+                value
+            }
+        }
+    }`;
+  }
+
+  /**
+   * @param {object} json
+   * @return {ResponseQueryUserActivity}
+   */
+  createResponse ( json ) {
+    return new ResponseQueryUserActivity( {
+      query: this,
+      json
+    } );
+  }
+
+}
